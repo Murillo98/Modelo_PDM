f3592bfb17ed8dcafbe2f78115e2721fc9ee7363
Murillo98 <86999104+Murillo98@users.noreply.github.com>
2021-07-09 01:44:48 -0500

    Update Bienvenido.php

--- Bienvenido.php
+++ Bienvenido.php
@@ -121,7 +121,7 @@ $password = "BxwlgwCvXX";
 $dbname = "ygEbfhzsHJ";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
